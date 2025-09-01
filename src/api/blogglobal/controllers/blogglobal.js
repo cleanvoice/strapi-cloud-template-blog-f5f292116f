3289d6d2cecf1ac516021d0f4c6659200abd23e9
Edi <dev@icebyte.io>
2025-09-01 13:10:21 +0700

    fix blog page not return seo metadata

--- src/api/blogglobal/controllers/blogglobal.js
+++ src/api/blogglobal/controllers/blogglobal.js
@@ -6,4 +6,23 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blogglobal.blogglobal');
+module.exports = createCoreController('api::blogglobal.blogglobal', ({ strapi }) => ({
+  async find(ctx) {
+    // Override the default find method to ensure metadata is populated
+    const entity = await strapi.entityService.findMany('api::blogglobal.blogglobal', {
+      populate: {
+        metadata: {
+          populate: '*'
+        }
+      }
+    });
+
+    // Since blogglobal is a single type, return the first (and only) entity
+    const blogglobal = Array.isArray(entity) ? entity[0] : entity;
+
+    return {
+      data: blogglobal,
+      meta: {}
+    };
+  }
+}));
